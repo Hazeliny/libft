31d3a64333350c1ae2465fa4cf51ef33707d646e
Lin Yao <linyao@cbr10s5.42barcelona.com>
2024-06-12 19:16:03 +0200

    libft 7

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: linyao <linyao@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 12:37:11 by linyao            #+#    #+#             */
-/*   Updated: 2024/06/03 13:11:47 by linyao           ###   ########.fr       */
+/*   Updated: 2024/06/12 15:29:21 by linyao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 //#include <stdio.h>
 //#include <stdlib.h>
 
-int ft_isspace(char c)
+int	ft_isspace(char c)
 {
-    if (c == 32 || (c >= 9 && c <= 13))
-        return (1);
-    return (0);
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
 }
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int     sign;
-    long    result;
+	int		sign;
+	long	result;
 
-    sign = 1;
-    result = 0;
-    while (ft_isspace(*str))
-        str++;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            sign = -1;
-        str++;
-    }
-    while (ft_isdigit(*str))
-    {
-        result = result * 10 + (*str - '0');
-        str++;
-    }
-    return (int)(sign * result);
+	sign = 1;
+	result = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (ft_isdigit(*str))
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return ((int)(sign * result));
 }
 /*
 int main(void)
---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linyao <linyao@student.42barcelona.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 15:54:36 by linyao            #+#    #+#             */
+/*   Updated: 2024/06/12 16:08:52 by linyao           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst && del)
+	{
+		del (lst->content);
+		free (lst);
+	}
+}
+
+void	del_content(void *content)
+{
+	free (content);
+}
